4d1e26f920b7b62af838f20ca79fe6764c25c6d6
Glowin <jiangbian66@gmail.com>
2024-05-20 22:50:55 +0800

    使用 .env 保存公钥和私钥

--- pump_fun_py/config.py
+++ pump_fun_py/config.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 from solana.rpc.api import Client
 from solders.keypair import Keypair #type: ignore
 
-PUB_KEY = "" # WALLET ADDRESS
-PRIV_KEY = "" # BASE58 STRING FORMAT
+load_dotenv()
+
+PUB_KEY = os.getenv("PUB_KEY") # WALLET ADDRESS
+PRIV_KEY = os.getenv("PRIV_KEY") # BASE58 STRING FORMAT
 RPC = "https://api.mainnet-beta.solana.com" # Use Helius or Quicknode for better performance
 client = Client(RPC)
 payer_keypair = Keypair.from_base58_string(PRIV_KEY)
