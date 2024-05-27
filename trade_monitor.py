3776276fddf37f718124b10221bd9996b5a9b02c
Glowin <jiangbian66@gmail.com>
2024-05-28 01:20:27 +0800

    fix bug

--- trade_monitor.py
+++ trade_monitor.py
@@ -5,13 +5,18 @@ from utils import get_trade_list
 db = MySQLDatabase()
 db.connect()
 
+# Argument parser
+parser = argparse.ArgumentParser(description='Process arguments')
+parser.add_argument('--proxy', default='None', help='proxy for pump_fun api, default is None')
+args = parser.parse_args()
+
 while(1):
     # Retrieve all mints that need to be checked for rugs
     mint_list = db.get_rug_checklist()
 
     # Record trade data for each mint in the database
     for mint_info in mint_list:
-        success = get_trade_list(mint_info[0], mint_info[1], mint_info[2])
+        success = get_trade_list(mint_info[0], mint_info[1], mint_info[2], proxy=args.proxy)
         if success:
             print(f"Successfully recorded trade data for symbol: {mint_info[2]}")
         else:
