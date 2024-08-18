3f7dad5d677695bd8c9e375d8de583ba85cd93aa
Glowin <jiangbian66@gmail.com>
2024-08-19 00:38:38 +0800

    修改 UA 试一下

--- utils.py
+++ utils.py
@@ -76,18 +76,16 @@ class Utils:
         headers = {
             'accept': '*/*',
             'accept-language': 'zh-CN,zh;q=0.9',
-            'cache-control': 'no-cache',
-            'origin': 'https://www.pump.fun',
-            'pragma': 'no-cache',
+            'origin': 'https://pump.fun',
             'priority': 'u=1, i',
-            'referer': 'https://www.pump.fun/',
-            'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            'referer': 'https://pump.fun/',
+            'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"macOS"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
         }
         if proxy and proxy != 'None':
             proxies = {
